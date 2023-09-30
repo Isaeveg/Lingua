689c5c86c04e54e46249e4f29ad8e370c1c99869
Isaeveg <isaev.egor.08@gmail.com>
2023-09-30 15:52:51 +0300

    add section teachers

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,18 @@
-// import header from './modules/header.js';
-// header();
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+});
